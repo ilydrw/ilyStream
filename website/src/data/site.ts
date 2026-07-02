5b1a3a1f0efd1198fd9f3161eb1c81f7dc54e125
ilydrw <drwdoesstuff@gmail.com>
2026-07-02 13:18:25 -0400

    Overhaul website docs and DeskThing release links

---
+++ website/src/data/site.ts
@@ -0,0 +1,11 @@
+export const latestVersion = '0.0.22';
+export const deskThingVersion = '0.0.22';
+
+export const repoUrl = 'https://github.com/ilydrw/ilyStream';
+export const releaseUrl = `${repoUrl}/releases/tag/v${latestVersion}`;
+export const setupUrl = `${repoUrl}/releases/download/v${latestVersion}/ilyStream-Setup-${latestVersion}.exe`;
+export const portableUrl = `${repoUrl}/releases/download/v${latestVersion}/ilyStream-Portable-${latestVersion}.exe`;
+
+export const deskThingRepoUrl = 'https://github.com/ilydrw/ilyStream-DeskThing';
+export const deskThingReleaseUrl = `${deskThingRepoUrl}/releases`;
+export const deskThingZipUrl = `${deskThingRepoUrl}/releases/download/v${deskThingVersion}/ilyStream-DeskThing-v${deskThingVersion}.zip`;
